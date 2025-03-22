bd7f22cb81ce26dd22542a0bfe47f4061623a0f1
xiaobaige <zitongbai@outlook.com>
2025-03-22 23:37:38 +0800

    gru exporter

--- legged_gym/utils/helpers.py
+++ legged_gym/utils/helpers.py
@@ -183,8 +183,12 @@ def get_args():
 def export_policy_as_jit(actor_critic, path):
     if hasattr(actor_critic, 'memory_a'):
         # assumes LSTM: TODO add GRU
-        exporter = PolicyExporterLSTM(actor_critic)
-        exporter.export(path)
+        if isinstance(actor_critic.memory_a.rnn, torch.nn.LSTM):
+            exporter = PolicyExporterLSTM(actor_critic)
+            exporter.export(path)
+        elif isinstance(actor_critic.memory_a.rnn, torch.nn.GRU):
+            exporter = PolicyExporterGRU(actor_critic)
+            exporter.export(path)
     elif hasattr(actor_critic, 'estimator'):
         exporter = PolicyExporterHIM(actor_critic)
         exporter.export(path)
@@ -220,6 +224,31 @@ class PolicyExporterLSTM(torch.nn.Module):
     def export(self, path):
         os.makedirs(path, exist_ok=True)
         path = os.path.join(path, 'policy_lstm_1.pt')
+        self.to('cpu')
+        traced_script_module = torch.jit.script(self)
+        traced_script_module.save(path)
+
+class PolicyExporterGRU(torch.nn.Module):
+    def __init__(self, actor_critic):
+        super().__init__()
+        self.actor = copy.deepcopy(actor_critic.actor)
+        self.is_recurrent = actor_critic.is_recurrent
+        self.memory = copy.deepcopy(actor_critic.memory_a.rnn)
+        self.memory.cpu()
+        self.register_buffer(f'hidden_state', torch.zeros(self.memory.num_layers, 1, self.memory.hidden_size))
+
+    def forward(self, x):
+        out, h = self.memory(x.unsqueeze(0), self.hidden_state)
+        self.hidden_state[:] = h
+        return self.actor(out.squeeze(0))
+
+    @torch.jit.export
+    def reset_memory(self):
+        self.hidden_state[:] = 0.
+ 
+    def export(self, path):
+        os.makedirs(path, exist_ok=True)
+        path = os.path.join(path, 'policy_gru_1.pt')
         self.to('cpu')
         traced_script_module = torch.jit.script(self)
         traced_script_module.save(path)
